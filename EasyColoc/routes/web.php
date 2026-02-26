e8e08cee616a92262a35abbcc56ad97b29bfa5c9
safaacht <safaachtaoui@gmail.com>
2026-02-26 14:00:32 +0000

    ajout des routes d'admin,owner,membre

--- EasyColoc/routes/web.php
+++ EasyColoc/routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ExpenseController;
 use App\Http\Controllers\SettlementController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\InvitationMail;
+use App\Exports\UsersExport;
+use App\Http\Controllers\UserController;
+use Maatwebsite\Excel\Facades\Excel;
+
+require __DIR__.'/auth.php';
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,9 +24,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
+// ==== Auth ==========
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
@@ -29,4 +39,26 @@ Route::middleware('auth')->group(function () {
     Route::patch('settlements/{settlement}/pay', [SettlementController::class, 'markAsPayed'])->name('settlements.pay');
 });
 
-require __DIR__.'/auth.php';
+// ======= Admin =======
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/dashboard',[AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::patch('/admin/users/{id}/ban',[AdminController::class, 'ban'])->name('admin.ban');
+    Route::patch('/admin/users/{id}/unban',[AdminController::class, 'unban'])->name('admin.unban');
+    Route::get('/export-users', function () {
+         return Excel::download(new UsersExport, 'users.xlsx');
+         })->name('admin.export');
+});
+
+
+// ======== Membre ========
+Route::middleware(['auth', 'membre'])->group(function () {
+    Route::post('/colocation/quitter', [ColocationController::class, 'quitter'])
+         ->name('colocation.quitter');
+});
+
+// ======= Owner ======
+Route::middleware(['auth', 'owner'])->group(function () {
+    Route::post('/colocation/invite',  [UserController::class, 'send'])->name('colocation.invite');
+    Route::delete('/colocation/{id}',  [ColocationController::class, 'destroy'])->name('colocation.cancel');
+});
