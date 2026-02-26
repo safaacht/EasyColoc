905b957b27beddfeda509c820a5d7d400507c6e3
safaacht <safaachtaoui@gmail.com>
2026-02-26 13:51:49 +0000

    ajouter un membre par invitation

--- EasyColoc/app/Http/Controllers/UserController.php
+++ EasyColoc/app/Http/Controllers/UserController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\InvitationMail;
+use App\Models\Colocation;
+use App\Models\Membership;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -21,4 +25,36 @@ class UserController extends Controller
 
     return redirect()->route('login');
 }
+
+
+public function addMember(Request $request,Colocation $colocation)
+{
+    $email=$request->email;
+    $user= User::where('email',$email)->first();
+
+    if(!$user){
+        return redirect()->back()
+        ->withErrors(['name'=>'User not found']);
+    }
+// colocation activity
+    $isActive=$user->colocations
+                   ->where('colocations.isActive',1)
+                   ->wherePivot('status','joined')
+    ;
+
+
+    $token=bin2hex(random_bytes(32));
+
+    Membership::create([
+        'user_id'         =>$user->id,
+        'colocation_id'   =>$colocation->id ,
+        'token'           =>$token,
+        'status'        => 'pending'
+    ]);
+
+    Mail::to($user->email)->send(new InvitationMail($token));
+}
+
+
+
 }
