222ff8848d688dad3d01b8b63d049b558ce4daa4
safaacht <safaachtaoui@gmail.com>
2026-02-26 14:03:12 +0000

    alias des middlewares

--- EasyColoc/bootstrap/app.php
+++ EasyColoc/bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'membre' => \App\Http\Middleware\MembreMiddleware::class,
+            'owner' => \App\Http\Middleware\OwnerMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
