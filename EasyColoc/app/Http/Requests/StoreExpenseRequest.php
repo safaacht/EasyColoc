e3bf0cc294015cdc6f036be6f7ec80c60fa603b2
safaacht <safaachtaoui@gmail.com>
2026-02-26 10:52:52 +0000

    Validation des inputes

---
+++ EasyColoc/app/Http/Requests/StoreExpenseRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreExpenseRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'amount' => 'required|numeric|min:1',
+            'category_id' => 'required|exists:category,id',
+        ];
+    }
+}
---
+++ EasyColoc/app/Http/Requests/UpdateColocationRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateColocationRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'address' => 'nullable|string|max:255',
+            'rules' => 'required|string',
+            'isActive' => 'boolean',
+        ];
+    }
+}
